6478e353091310b55f81350f68d592dfb1474401
marius david <mariusdavid@laposte.net>
2020-12-03 10:50:08 +0100

    split the configuration display in another file

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,2 +1,26 @@
 mod application;
-pub use application::{Flags, Message, NixMiniGuiApp};
+pub use application::{Flags, NixMiniGuiApp};
+
+mod displayed_configuration;
+pub use displayed_configuration::DisplayedConfiguration;
+
+
+
+
+use crate::config_manager::ConfigManager;
+pub struct AppSetting {
+    pub config_manager: ConfigManager,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    SwitchScreenInstallNew,
+    SwitchScreenManageConfig,
+    SelectedPotentialInstallTarget(String),
+    EnableConfig(String),
+    DisableConfig(String),
+    ConfigurePackage(String),
+    SetConfiguration(String, String, String), //config key, id, value
+    Ignore,
+    Todo,
+}
